a2d5e813c3495225ba693ea2794c8d1bea397484
Krishna Kant Verma <krishna6431@users.noreply.github.com>
2021-08-08 21:18:43 +0530

    added assignment-5

---
+++ WebTechnology-Lab-Assignment/Lab-Assignment-5/js/script.js
@@ -0,0 +1,14 @@
+// < !-- /* // < !--this project is created by krishna6431 */ -->
+function weightConverter(valNum) {
+    var valNum = valNum * 2.2046;
+    var n = valNum.toFixed(0);
+    document.getElementById("outputPounds").innerHTML = n;
+}
+
+function weightConverter2(valNum) {
+    var valNum = valNum / 2.2046;
+    var n = valNum.toFixed(0);
+    document.getElementById("outputKilograms").innerHTML = n;
+}
+
+document.getElementById("copyright").innerHTML = "Copyright © Krishna. All Rights Reserved."
